2819adfbc12d2b065e194c03f6d20090501f6194
NutNatcha <natcha_me@cmu.ac.th>
2022-08-04 13:59:38 +0700

    icon

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const genderIcon = document.querySelector("#span-gender-icon");
+const imgProfile = document.querySelector("#img-profile");
+const pName = document.querySelector("#p-name");
+const pAddress = document.querySelector("#p-address");
+const pEmail = document.querySelector("#p-email");
+const btnRandom = document.querySelector("#btn-random");
+const loadingCard = document.querySelector("#div-loading-card");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+}
+
+if (resp.data.results.gender == female) {
+  genderIcon.innerText = "👩";
+} else {
+  genderIcon.innerText = "👨";
 }
